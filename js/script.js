53ed882ba213392f6e9dc00aa8ef1d2cfc6a3f9a
Cole <122782745+cm-thom@users.noreply.github.com>
2023-06-13 16:58:09 -0400

    api call to get coords functioning

--- js/script.js
+++ js/script.js
@@ -1,27 +1,38 @@
-const apiKey = "4f3c7240a8f0d8d3b933b7148a846627";
+const apiKey = '2a70c788252f5dd21f3f2bcf99e3b20b';
+
 
 //give a city name, get coordinates
 const getLocation = (city) => {
-    `fetch(api.openweathermap.org/geo/1.0/direct?q={city}&limit=1&appid={apiKey})`
-}
+    return `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`
+};
 
-//give coordinates, get 5 day forecast
-// const getWeather = ([latitude, longitude]) => {
-//     fetch(api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key})
-    
-// }
+
 
 //takes the content of search box, runs getLocation to get coords, then runs getWeather with coords to return forecast
 const getLocWeather = () => {};
 
 
-
 const weatherBtnEl = document.getElementById('search-btn');
+const searchInputEl = document.getElementById('search-input');
 
 weatherBtnEl.addEventListener( "click", (e) => {
     e.preventDefault();
-    console.log('button works')
+    fetch(getLocation(searchInputEl.value))
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data)
+        const latitude = data[0].lat;
+        const longitude = data[0].lon;
+        return [latitude, longitude];
+    })
 })
+
+//give coordinates, make call to current weather api, get info, update content of html card
+// const getWeather = ([latitude, longitude]) => {
+//     fetch(api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key})
+    
+// }
+
 
 
 
