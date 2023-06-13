d1d05332ea1f9294b580f19dbea592d60d6b2576
Cole <122782745+cm-thom@users.noreply.github.com>
2023-06-13 17:59:27 -0400

    functional. updates card with proper info, some calcs to make data digestible

--- js/script.js
+++ js/script.js
@@ -1,39 +1,47 @@
 const apiKey = '2a70c788252f5dd21f3f2bcf99e3b20b';
 
+const weatherBtnEl = document.getElementById('search-btn');
+const searchInputEl = document.getElementById('search-input');
+
+const cityEl = document.getElementById('cityEl');
+const tempEl = document.getElementById('tempEl');
+const windEl = document.getElementById('windEl');
+const humidityEl = document.getElementById('humidityEl');
+
 
 //give a city name, get coordinates
 const getLocation = (city) => {
     return `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`
 };
 
+//give coordinates, make call to current weather api
+const getWeather = (coords) => {
+    return `http://api.openweathermap.org/data/2.5/weather?lat=${coords[0]}&lon=${coords[1]}&appid=${apiKey}`  
+};
 
+const updateCard = (data) => {
+    cityEl.innerHTML = data.name;
+    const fTemp = Math.round((data.main.temp - 273.15) * (9/5) + 32)
+    tempEl.innerHTML = `Temperature: ${fTemp}`;
+    windEl.innerHTML = `Wind Speed: ${data.wind.speed} m/sec`;
+    humidityEl.innerHTML = `Humidity: ${data.main.humidity}%`;
+};
 
-//takes the content of search box, runs getLocation to get coords, then runs getWeather with coords to return forecast
-const getLocWeather = () => {};
-
-
-const weatherBtnEl = document.getElementById('search-btn');
-const searchInputEl = document.getElementById('search-input');
 
 weatherBtnEl.addEventListener( "click", (e) => {
     e.preventDefault();
-    fetch(getLocation(searchInputEl.value))
+     fetch(getLocation(searchInputEl.value))
     .then((res) => res.json())
     .then((data) => {
         console.log(data)
         const latitude = data[0].lat;
         const longitude = data[0].lon;
-        return [latitude, longitude];
+        const coords = [latitude, longitude];
+    fetch(getWeather(coords))
+        .then((response) => response.json())
+        .then((data) => {
+            updateCard(data);
+        //run the function to update the html content
+        })
     })
 })
-
-//give coordinates, make call to current weather api, get info, update content of html card
-// const getWeather = ([latitude, longitude]) => {
-//     fetch(api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key})
-    
-// }
-
-
-
-
-//set the content of the divs by targeting by id then assign content with innerHtml or whatever the right one is
